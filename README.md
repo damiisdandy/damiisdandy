89f252ba8ff64124e4c68b3108be1ad4897f64fb
Damilola Jerugba <damiisdandy@icloud.com>
2021-07-21 19:01:29 +0100

    Added Typescript and ChakraUI

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 
 <p align="left"> <img src="https://komarev.com/ghpvc/?username=damiisdandy&label=Profile%20views&color=0e75b6&style=flat" alt="damiisdandy" /> </p>
 
-<p align="left"> <a href="https://github.com/ryo-ma/github-profile-trophy"><img src="https://github-profile-trophy.vercel.app/?username=damiisdandy" alt="damiisdandy" /></a> </p>
 
 <p align="left"> <a href="https://twitter.com/damiisdandy" target="blank"><img src="https://img.shields.io/twitter/follow/damiisdandy?logo=twitter&style=for-the-badge" alt="damiisdandy" /></a> </p>
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,21 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
+      resolve: "@chakra-ui/gatsby-plugin",
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
